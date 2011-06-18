caa35a8c2951d98bd81d8fb4e343f6709f1413a2
Austin Duve <cohesioN241@gmail.com>
2011-06-18 11:27:48 -0500

    Bug Fix : taxi map not displaying.

--- Tukui/modules/skins/taxi.lua
+++ Tukui/modules/skins/taxi.lua
@@ -3,7 +3,7 @@ local T, C, L = unpack(select(2, ...))
 local function LoadSkin()
 	TaxiFrame:StripTextures()
 	TaxiFrame:CreateBackdrop("Transparent")
-	TaxiRouteMap:CreateBackdrop("Default")
+	TaxiRouteMap:CreateBackdrop("Transparent")
 	TaxiRouteMap.backdrop:SetAllPoints()
 	T.SkinCloseButton(TaxiFrameCloseButton)
 end
