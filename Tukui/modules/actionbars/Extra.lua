8e1261f0d5b65807c84514878aef653519fc2b7e
Austin Duve <cohesioN241@gmail.com>
2011-12-02 01:13:47 -0600

    Reskin Extra Button to match Tukui.

--- Tukui/modules/actionbars/Extra.lua
+++ Tukui/modules/actionbars/Extra.lua
@@ -1,20 +1,43 @@
 local T, C, L = unpack(select(2, ...))
 if T.toc < 40300 then return end
 
--- create the anchor to allow moving extra button
-local anchor = CreateFrame("Frame", "TukuiExtraActionBarFrameHolder", UIParent)
-anchor:Size(160, 80)
-anchor:SetPoint("BOTTOM", 0, 250)
-anchor:SetMovable(true)
-anchor:SetTemplate("Default")
-anchor:SetBackdropBorderColor(0,0,0,0)
-anchor:SetBackdropColor(0,0,0,0)
-anchor.text = T.SetFontString(anchor, unpack(T.Fonts.movers.setfont))
-anchor.text:SetPoint("CENTER")
-anchor.text:SetText(L.move_extrabutton)
-anchor.text:Hide()
+-- create the holder to allow moving extra button
+local holder = CreateFrame("Frame", "TukuiExtraActionBarFrameHolder", UIParent)
+holder:Size(160, 80)
+holder:SetPoint("BOTTOM", 0, 250)
+holder:SetMovable(true)
+holder:SetTemplate("Default")
+holder:SetBackdropBorderColor(0,0,0,0)
+holder:SetBackdropColor(0,0,0,0)	
+holder.text = T.SetFontString(holder, C.media.uffont, 12)
+holder.text:SetPoint("CENTER")
+holder.text:SetText(L.move_extrabutton)
+holder.text:Hide()
 
 -- We never use MainMenuBar, so we need to parent this frame outside of it else it will not work.
-ExtraActionBarFrame:SetParent(anchor)
+ExtraActionBarFrame:SetParent(holder)
 ExtraActionBarFrame:ClearAllPoints()
-ExtraActionBarFrame:SetPoint("CENTER", anchor, "CENTER", 0, 0)
+ExtraActionBarFrame:SetPoint("CENTER", holder, "CENTER", 0, 0)
+
+-- hook the texture, idea by roth via WoWInterface forums
+local button = ExtraActionButton1
+local icon = button.icon
+local texture = button.style
+local disableTexture = function(style, texture)
+	if string.sub(texture,1,9) == "Interface" then
+		style:SetTexture("")
+	end
+end
+button.style:SetTexture("")
+hooksecurefunc(texture, "SetTexture", disableTexture)
+
+-- spell icon
+icon:SetTexCoord(.08, .92, .08, .92)
+icon:Point("TOPLEFT", button, 2, -2)
+icon:Point("BOTTOMRIGHT", button, -2, 2)
+
+-- pushed/hover
+button:StyleButton(true)
+
+-- backdrop
+button:SetTemplate("Default")
