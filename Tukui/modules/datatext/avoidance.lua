6fa6c88380bfaa214b5033a0337a982b7596a4b0
Austin Duve <cohesioN241@gmail.com>
2011-12-10 16:17:10 -0600

    This should be local

--- Tukui/modules/datatext/avoidance.lua
+++ Tukui/modules/datatext/avoidance.lua
@@ -15,12 +15,12 @@ if C["datatext"].avd and C["datatext"].avd > 0 then
 	Text:SetFont(unpack(T.Fonts.dFont.setfont))
 	T.PP(C["datatext"].avd, Text)
 	
-	local targetlv
-	local playerlv
+	local targetlv, playerlv, dodge, parry, block, MissChance
 
 	local function Update(self)
 		local format = string.format
 		targetlv, playerlv = UnitLevel("target"), UnitLevel("player")
+		local basemisschance, leveldifference, avoidance
 		
 		if targetlv == -1 then
 			basemisschance = (5 - (3*.2))  --Boss Value
